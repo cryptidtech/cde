0996a999e04ee044f21bfc0001820faace1f6825
Dave Huseby <dave@cryptid.tech>
2021-09-20 15:25:25 -0700

    major restructuring to support macros
    Signed-off-by: Dave Huseby <dave@cryptid.tech>

--- core/cli/src/bin/cde.rs
+++ core/cli/src/bin/cde.rs
@@ -12,7 +12,7 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(
     name = "cde",
-    version = "0.0.2",
+    version = "0.1.1",
     author = "David Huseby <dwh@linuxprogrammer.org>",
     about = "Encode/Decode cryptographic data in CDE format",
 )]
---
+++ core/codegen/src/lib.rs
@@ -0,0 +1,7 @@
+/*
+#[proc_macro_attribute]
+pub fn cde(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let attr_ast = syn::parse(attr).unwrap();
+    let item_ast = syn::parse(item).unwrap();
+}
+*/
